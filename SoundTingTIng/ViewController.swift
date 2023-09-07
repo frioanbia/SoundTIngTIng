ffb38f17a338d1e763358e3dbc43d9bc95b898ba
frio anbia <frioanbia63@gmail.com>
2023-09-07 09:35:02 +0700

    change design gamelan

--- SoundTingTIng/ViewController.swift
+++ SoundTingTIng/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
         sender.alpha = 0.5
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             sender.alpha = 1.0
         }
     }
@@ -30,7 +31,6 @@ class ViewController: UIViewController {
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
-    
     
 }
 
